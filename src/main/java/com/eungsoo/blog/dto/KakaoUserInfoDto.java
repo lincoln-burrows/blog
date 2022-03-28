279c981c0ac32075052ff146a81dd76ab73572d9
lincoln-burrows <minseong.kim7000@gmail.com>
2022-03-28 16:18:11 +0900

    지난과제샘플코드+심화수업코드 병합

---
+++ src/main/java/com/eungsoo/blog/dto/KakaoUserInfoDto.java
@@ -0,0 +1,16 @@
+package com.eungsoo.blog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class KakaoUserInfoDto {
+    private Long id;
+    private String nickname;
+    private String email;
+
+
+
+}
+
